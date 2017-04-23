1aca56561b14a31d9840f22a18cebdc5847eac69
Aggrathon <antonbjo@gmail.com>
2017-04-23 03:51:26 +0300

    Intro Scene

---
+++ Assets/IntroController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour {
+
+	public Transform mainCamera;
+
+	public void NextScreen(Transform screen)
+	{
+		StartCoroutine(Utility.Jump(mainCamera, screen.position, 1f, null, 2f));
+	}
+
+	public void LoadLevel(string level)
+	{
+		SceneManager.LoadScene(level);
+	}
+}
